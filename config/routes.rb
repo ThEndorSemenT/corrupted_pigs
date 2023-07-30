4bc9f01a15b70ee493b58a944256dee8fd30b0e1
Nuno Costa <nuno.mmc@gmail.com>
2023-07-30 17:14:19 +0100

    Add Stripe crypto plans (test mode)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/plans', to: 'home#plans'
+  get '/crypto/plans', to: 'home#crypto_plans'
 
   # Defines the root path route ("/")
   root "home#index"
